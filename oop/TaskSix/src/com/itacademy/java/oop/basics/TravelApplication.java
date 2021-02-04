03f6f786d6f9e9b23c78e322e8cc5803cd1bd24a
Arnas Čižikovas <arnas.cizikovas@gmail.com>
2021-02-04 10:18:56 +0200

    OOP I Task 6

--- oop/TaskSix/src/com/itacademy/java/oop/basics/TravelApplication.java
+++ oop/TaskSix/src/com/itacademy/java/oop/basics/TravelApplication.java
@@ -5,7 +5,7 @@ public class TravelApplication {
     public static class TravelManager {
 
         public static Family changeDestination(Family family, TravelDestination destination) {
-            if(family.getTravelDestination() == destination) {
+            if (family.getTravelDestination() == destination) {
                 return family;
             } else {
                 family.setTravelDestination(destination);
@@ -16,7 +16,7 @@ public class TravelApplication {
         public static void travel(Family family) {
             double carTravelDistance = family.getVehicle().maxTravelDistance();
             double destinationDistance = family.getTravelDestination().getDistance();
-            if(carTravelDistance >= destinationDistance) {
+            if (carTravelDistance >= destinationDistance) {
                 System.out.println("The family is able to travel to their destination");
             } else {
                 double remainingDistance = destinationDistance - carTravelDistance;
@@ -26,7 +26,7 @@ public class TravelApplication {
         }
 
         public static void printFamilyMembers(Family family) {
-            for (Person person: family.getPeople()) {
+            for (Person person : family.getPeople()) {
                 System.out.println(person);
             }
         }
@@ -42,7 +42,7 @@ public class TravelApplication {
         Vehicle mini = new Vehicle("Mini", "Cooper", 36.2, 8.1);
         TravelDestination klaipeda = new TravelDestination("Klaipėdos rajonas", "Klaipėda", 315.00);
         TravelDestination alytus = new TravelDestination("Alytaus rajonas", "Alytus", 86.00);
-        Family doe = new Family(new Person[]{john,jane}, mini, alytus);
+        Family doe = new Family(new Person[]{john, jane}, mini, alytus);
         Family jones = new Family(new Person[]{bob, sam}, fiat, alytus);
         TravelManager.printFamilyMembers(doe);
         System.out.println(doe);
